c2ad98c9c4cdaa28a478de5f858f6edc3ee1ffab
jaimatadi29 <sarita.mishra2924@outlook.com>
2025-04-07 22:22:16 +0530

    [07-04-2025] First step to start chaiCode landing page project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+         Chai Code
+      </h1>
+    </>
+  )
+}
+
+export default App
